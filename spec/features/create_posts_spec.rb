26ade5c40cc43ba2774c3e674d314165ca989848
Tatiana Nantes <tatiananantes@gmail.com>
2021-12-11 23:51:41 +0000

    show and new page created - can see specific post on the page

--- spec/features/create_posts_spec.rb
+++ spec/features/create_posts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-feature 'Creates a post' do
-  scenario 'can create a post' do
+RSpec.feature "Submit posts", type: :feature do
+  scenario "Can submit posts and view them" do
     visit '/'
     click_link 'New Post'
     # attach_file('Image', 'spec/files/images/Mel.jpg')
@@ -11,3 +11,4 @@ feature 'Creates a post' do
     # # expect(page).to have_css("img[src*='mel.jpg']")
   end
 end
+
