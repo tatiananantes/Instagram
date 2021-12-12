1362c481b37d49742fe2c0cc5946087503f4a97b
Tatiana Nantes <tatiananantes@gmail.com>
2021-12-12 11:53:01 +0000

    implement Active Storage but images are not displaying correctly yet

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,14 @@
 class PostsController < ApplicationController
   
+  def new
+    @post = Post.new
+  end
+
+  def create 
+    Post.create(post_params)
+    redirect_to posts_path
+  end
+
   def index
     @posts = Post.all
   end
@@ -7,4 +16,11 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+ 
+private 
+
+def post_params
+  params.require(:post).permit(:image, :caption)
+end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+  validates :caption, presence: true
+  validates :image, presence: true
+  
+  has_one_attached :image
+  # def image_presence
+  #   errors.add(:image, "can't be blank") unless image.attached?
+  # end
 end
